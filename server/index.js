4a2cc627f6b99720c19568dcc70f4a320ea3334d
Parikhit <parikhitritzz@gmail.com>
2024-04-09 02:19:49 +0530

    cors resolved

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: 'http://localhost:3000' || 'https://parikhits-portfolio-api.vercel.app/',
+        origin: 'https://parikhits-portfolio-api.vercel.app/',
     })
 );
 
