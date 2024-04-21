3532fb0f16c317cd9c5ed1cadd514ff3e4eb9ea2
Parikhit <parikhitritzz@gmail.com>
2024-04-21 16:37:27 +0530

    Fixed Issues

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: 'https://localhost:3000',
+        origin: 'https://parikhit-baruah97.vercel.app',
     })
 );
 
