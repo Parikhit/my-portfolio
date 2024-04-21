3103a11bcf0d10ccb02af429e634febcbd39b5c5
Parikhit <parikhitritzz@gmail.com>
2024-04-21 14:05:02 +0530

    Connecting BE with FE

--- client/components/custom/Contact.component.jsx
+++ client/components/custom/Contact.component.jsx
@@ -1,7 +1,7 @@
 import { Button } from '../ui/button';
 import { useState } from 'react';
 
-const API_URL = 'https://my-portfolio-api-q606.onrender.com/';
+const API_URL = 'http://localhost:8000/';
 
 const Contact = () => {
     const [disabledButton, setDisabledButton] = useState(false);
