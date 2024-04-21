ca6afb5882f3d11e51d15ec8aba8900043f67f0a
Parikhit <parikhitritzz@gmail.com>
2024-04-21 15:40:45 +0530

    Hiding default scrollbar

--- client/components/custom/Contact.component.jsx
+++ client/components/custom/Contact.component.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
         setDisabledButton(true);
         setLoading(true);
         try {
-            const res = await fetch('http://localhost:8000', {
+            const res = await fetch('https://my-portfolio-api-q606.onrender.com', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
