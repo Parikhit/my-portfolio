173415b5fe00c9ddb7bb6a0036e7be2b361f7937
Parikhit <parikhitritzz@gmail.com>
2024-05-04 10:41:13 +0530

    Fixing issues

--- components/custom/Projects.component.tsx
+++ components/custom/Projects.component.tsx
@@ -11,9 +11,12 @@ const Projects = () => {
     const onPressHandler = () => setProjectCards(<ProjectCards />);
 
     return (
-        <div id='projects' className='relative z-10 max-w-5xl mx-auto px-8 mb-44'>
+        <div
+            id='projects'
+            className='relative z-10 max-w-5xl mx-auto px-8 mb-44'
+        >
             <div className='absolute -z-10 w-full top-80 right-8 flex justify-end items-start'>
-                <div className='w-[400px] h-[400px] sm:w-[600px] sm:h-[600px] bg-gradient-to-b from-[#f539aa] to-[#fe6c4c] rounded-full blur-[80px] sm:animate-fourth sm:blur-[200px]'></div>
+                <div className='w-[400px] h-[400px] sm:w-[600px] sm:h-[600px] bg-gradient-to-b from-[#f539aa] to-[#fe6c4c] rounded-full blur-[80px] sm:blur-[200px]'></div>
             </div>
 
             <h1 className='text-rose-700 text-4xl text-center font-extrabold '>Projects</h1>
